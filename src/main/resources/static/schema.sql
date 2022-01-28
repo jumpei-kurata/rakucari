e6a11b469e7e46b22c37bed76298ef80b80b9a5a
jumpei-kurata <jumpei.kurata@rakus-partners.co.jp>
2022-01-28 13:04:01 +0900

    ログインフォームの追加 スキーマ作成 mysql接続の設定をアプリケーションヤマルに追加

---
+++ src/main/resources/static/schema.sql
@@ -0,0 +1,15 @@
+--usersÇ™Ç†ÇÍÇŒçÌèú
+DROP TABLE IF EXISTS booktable;
+
+--usersÇ™Ç»ÇØÇÍÇŒêVÇµÇ≠çÏê¨
+CREATE TABLE IF NOT EXISTS users(
+id INT PRIMARY KEY AUTO_INCREMENT,
+administrator INT DEFAULT 10,
+name VARCHAR(50) NOT NULL,
+nick_name VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL,
+zipcode VARCHAR(8) NOT NULL,
+address VARCHAR(200) NOT NULL,
+telephone VARCHAR(15) NOT NULL,
+pass VARCHAR(50) NOT NULL,
+);
